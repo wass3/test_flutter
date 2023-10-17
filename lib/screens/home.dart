9a58921f73cc17faf3fce5b3cafe2c5b0b000a94
wassim <unlessbbx03@gmail.com>
2023-10-17 09:39:50 +0200

    a ver

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,7 +9,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text("Pruesdba"),
         ),
       ),
     );
